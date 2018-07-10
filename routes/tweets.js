ccad0e078478c5caa84af774de3f3fbaed563b78
Mohammed Yehia <m-ya@windowslive.com>
2018-07-10 11:21:54 +0300

    fix the routes

--- routes/tweets.js
+++ routes/tweets.js
@@ -4,17 +4,17 @@ const Twit = require('twit');
 const twitConfig = require('./connections/twitConfig');
 const Twitter = new Twit(twitConfig);
 
-router.get('/tweets/:word', function (req, res, next) {
-    Twitter.get('search/tweets', {
-                q: req.params.word,
-                count: 2
-            }, function (err, tweets, response) {
-        if(err) console.log(err)
-        res.json(tweets)
-    })
-});
+// router.get('/tweets/:word', function (req, res, next) {
+//     Twitter.get('search/tweets', {
+//                 q: req.params.word,
+//                 count: 2
+//             }, function (err, tweets, response) {
+//         if(err) console.log(err)
+//         res.json(tweets)
+//     })
+// });
 
-router.get('/tweets/:user', function (req, res, next) {
+router.get('/usertweets/:user', function (req, res, next) {
     Twitter.get('statuses/user_timeline', {
                 screen_name: req.params.user,
                 count: 50,
