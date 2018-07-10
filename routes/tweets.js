a26955d23413ef4bfaf54878d14fc61199769961
Mohammed Yehia <m-ya@windowslive.com>
2018-07-10 22:39:58 +0300

    re-structre the json data

--- routes/tweets.js
+++ routes/tweets.js
@@ -1,5 +1,4 @@
-const express = require('express');
-const router = express.Router();
+const router = require('express').Router();
 const Twit = require('twit');
 const twitConfig = require('./connections/twitConfig');
 const Twitter = new Twit(twitConfig);
@@ -7,7 +6,7 @@ const Twitter = new Twit(twitConfig);
 // router.get('/tweets/:word', function (req, res, next) {
 //     Twitter.get('search/tweets', {
 //                 q: req.params.word,
-//                 count: 2
+//                 count: 2,
 //             }, function (err, tweets, response) {
 //         if(err) console.log(err)
 //         res.json(tweets)
@@ -17,13 +16,26 @@ const Twitter = new Twit(twitConfig);
 router.get('/usertweets/:user', function (req, res, next) {
     Twitter.get('statuses/user_timeline', {
                 screen_name: req.params.user,
-                count: 50,
-                // trim_user:1,
-                include_entities:false
+                count: 30,
+                tweet_mode:'extended',
+                include_entities:false,
+                // include_rts:false,
+                // exclude_replies:true,
             }, function (err, tweets, response) {
-        if(err) console.log(err)
-        res.json(tweets)
+                if(err) console.log(err)
+                const filteredTweets = tweets.map(tweet => {
+                    return {
+                        created_at: tweet.created_at,
+                        text: tweet.full_text,
+                        profile_banner_url: tweet.user.profile_banner_url,
+                        profile_image_url: tweet.user.profile_image_url
+                    }
+                });
+            res.json(filteredTweets)
     })
 });
+
+
+
 
 module.exports = router;
