f3056f797689eca53faa09ad9520509ed1ae8eb0
Mohammed Yehia <m-ya@windowslive.com>
2018-07-10 11:12:25 +0300

    the main structure with main files and twitter api

---
+++ routes/tweets.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const router = express.Router();
+const Twit = require('twit');
+const twitConfig = require('./connections/twitConfig');
+const Twitter = new Twit(twitConfig);
+
+router.get('/tweets/:word', function (req, res, next) {
+    Twitter.get('search/tweets', {
+                q: req.params.word,
+                count: 2
+            }, function (err, tweets, response) {
+        if(err) console.log(err)
+        res.json(tweets)
+    })
+});
+
+router.get('/tweets/:user', function (req, res, next) {
+    Twitter.get('statuses/user_timeline', {
+                screen_name: req.params.user,
+                count: 50,
+                // trim_user:1,
+                include_entities:false
+            }, function (err, tweets, response) {
+        if(err) console.log(err)
+        res.json(tweets)
+    })
+});
+
+module.exports = router;
